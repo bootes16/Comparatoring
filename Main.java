922c37f9fc38413f9b708131f404ae9dbefb1e35
Greg Johnson <gjjohnson@gmail.com>
2021-01-22 01:33:36 +0000

    Refactor to use IntStream etc.

--- Main.java
+++ Main.java
@@ -1,4 +1,6 @@
 import java.util.*;
+import java.util.function.IntSupplier;
+import java.util.stream.IntStream;
 
 class Cptor implements Comparator<Integer> {
   public int compare(Integer a, Integer b) {
@@ -6,20 +8,23 @@ class Cptor implements Comparator<Integer> {
   }
 }
 
+class Fibs implements IntSupplier {
+  private int a = 1;
+  private int b = 0;
+  public int getAsInt() {
+    int r = a + b;
+    a = b;
+    b = r;
+    return r;
+  }
+}
 
 class Main {
   public static void main(String[] args) {
     
-    int arraySize = 10;
-    Integer[] ra = new Integer[arraySize];
-    int a = 1;
-    int b = 0;
-    for (int i=0; i < arraySize; i++) {
-      ra[i] = a + b;
-      int t = b;
-      b += a;
-      a = t;
-    }
+    IntSupplier isup = new Fibs();
+    Integer[] ra = IntStream.generate(isup).limit(10).boxed().toArray(Integer[]::new);
+    System.out.println(Arrays.toString(ra));
     
     Comparator<Integer> cp = new Cptor();
     Arrays.sort(ra, cp);
@@ -27,7 +32,5 @@ class Main {
 
     Arrays.sort(ra, (Integer aa, Integer bb) -> aa - bb);
     System.out.println(Arrays.toString(ra));
-
-    System.out.println(Arrays.toString(new Integer[]{1,2,3}));
   }
 }
